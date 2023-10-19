3ad43fd036ac884bed6e58aef74d93755a367bb7
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-19 16:28:47 +0200

    ft: mi collego al model book e uso faker

--- database/seeders/BooksTableSeeders.php
+++ database/seeders/BooksTableSeeders.php
@@ -2,6 +2,13 @@
 
 namespace Database\Seeders;
 
+/* 
+ * mi aggaincio al model di books */
+use App\Models\Book;
+/* 
+ * uso Faker per generare dei finti libri */
+use Faker\Generator as Faker;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,7 +19,7 @@ class BooksTableSeeders extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
 
     }

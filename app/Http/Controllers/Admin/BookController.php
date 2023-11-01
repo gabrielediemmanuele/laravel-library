ad2150e785f2c48322d17367371cb7c6d7f63661
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-11-01 16:44:41 +0100

    feat: add a global validator (store + update)

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -16,24 +16,17 @@ class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * return \Illuminate\Http\Response
      */
     public function index()
     {
-
         /* Invio alla route i dati: titolo e tutti i libri presi dal model (migration + seeder) */
-
         $title = "Books";
         $books = Book::all();
         return view('admin.books.index', compact('books', 'title'));
     }
 
-
     /**
      * Show the form for creating a new resource.
-     *
-     * * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -43,53 +36,15 @@ class BookController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
+        /* validation call */
+        $data = $this->validation($request->all());
         /* creo un nuovo libro */
         $book = new Book();
-
-        $data = $request->all();
-
-        $validator = Validator::make(
-            $data,
-            [
-                'title' => 'required|string|max:50',
-                'author'=>'required',
-                'price' => 'required',
-                'genre' => 'required|string',
-                'editor_house' => 'required|string|max:30',
-                'pages' => 'required',
-                'edition' => 'required',
-                'series_number' => 'required',
-                'copies_number' => 'required',
-                'genre_id'=>'required',
-            ],
-            [
-                'title.required' => 'Title is required',
-                'title.string' => 'Title need to be a string',
-                'title.max' => 'Title is max 50 char',
-                
-                'author.required'=>'Author is required',
-                'price.required' => 'Price is required',
-                'genre.required' => 'Genre is required',
-                'editor_house.required' => 'Editor house is required',
-                'pages.required' => 'Pages is required',
-                'edition.required' => 'edition is required',
-                'series_number.required' => 'Series number is required',
-                'copies_number.required' => 'Copies number is required',
-                'genre_id.required'=>'Type is required'
-            ],
-        );
-
-
-    
         /* lo riempo di informazioni */
         $book->fill($data);
-
         /* le salvo nel database */
         $book->save();
 
@@ -99,12 +54,8 @@ class BookController extends Controller
         return redirect()->route('admin.books.show', $book);
     }
 
-
     /**
      * Display the specified resource.
-     *
-     * @param  int  $id
-     * * @return \Illuminate\Http\Response
      */
     public function show(Book $book)
     {
@@ -114,59 +65,20 @@ class BookController extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * return \Illuminate\Http\Response
      */
     public function edit(Book $book)
     {
         $genres = Genre::all();
-        return view('admin.books.edit', compact('book','genres'));
+        return view('admin.books.edit', compact('book', 'genres'));
     }
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * return \Illuminate\Http\Response
      */
     public function update(Request $request, Book $book)
     {
-        $data = $request->all();
-        
-        //VALIDATOR 
-        $validator = Validator::make(
-            $data,
-            [
-                'title' => 'required|string|max:50',
-                'author'=>'required',
-                'price' => 'required',
-                'genre' => 'required|string',
-                'editor_house' => 'required|string|max:30',
-                'pages' => 'required',
-                'edition' => 'required',
-                'series_number' => 'required',
-                'copies_number' => 'required',
-                'genre_id'=>'required',
-            ],
-            [
-                'title.required' => 'Title is required',
-                'title.string' => 'Title need to be a string',
-                'title.max' => 'Title is max 50 char',
-                
-                'author.required'=>'Author is required',
-                'price.required' => 'Price is required',
-                'genre.required' => 'Genre is required',
-                'editor_house.required' => 'Editor house is required',
-                'pages.required' => 'Pages is required',
-                'edition.required' => 'edition is required',
-                'series_number.required' => 'Series number is required',
-                'copies_number.required' => 'Copies number is required',
-                'genre_id.required'=>'Type is required'
-            ],
-        );
-
+        /* Validator -> riga 134 */
+        $data = $this->validation($request->all());
 
         $book->update($data);
         return redirect()->route('admin.books.show', $book);
@@ -174,13 +86,48 @@ class BookController extends Controller
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * return \Illuminate\Http\Response
      */
     public function destroy(Book $book)
     {
         $book->delete();
         return redirect()->route('admin.books.index');
+    }
+
+    /*
+     * VALIDATOR sia per Update che per Store
+     */
+    private function validation($data)
+    {
+        $validator = Validator::make(
+            $data,
+            [
+                'title' => 'required|string|max:50',
+                'author' => 'required',
+                'price' => 'required',
+                'genre' => 'required|string',
+                'editor_house' => 'required|string|max:30',
+                'pages' => 'required',
+                'edition' => 'required',
+                'series_number' => 'required',
+                'copies_number' => 'required',
+                'genre_id' => 'required',
+            ],
+            [
+                'title.required' => 'Title is required',
+                'title.string' => 'Title need to be a string',
+                'title.max' => 'Title is max 50 char',
+
+                'author.required' => 'Author is required',
+                'price.required' => 'Price is required',
+                'genre.required' => 'Genre is required',
+                'editor_house.required' => 'Editor house is required',
+                'pages.required' => 'Pages is required',
+                'edition.required' => 'edition is required',
+                'series_number.required' => 'Series number is required',
+                'copies_number.required' => 'Copies number is required',
+                'genre_id.required' => 'Type is required'
+            ],
+        )->validate();
+        return $validator;
     }
 }

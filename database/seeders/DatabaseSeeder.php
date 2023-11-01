1c4ada185111af9b1a6513634744a0d5bcfa2c61
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-11-01 17:06:34 +0100

    fix: clean and comments seeders + index.blade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,19 +8,14 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
-     *
-     * @return void
+     ** Seed the application's database.
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call([UserSeeder::class, GenreSeeder::class, BooksTableSeeders::class ]);
-
+        $this->call([
+            UserSeeder::class,
+            GenreSeeder::class,
+            BooksTableSeeders::class
+        ]);
     }
 }

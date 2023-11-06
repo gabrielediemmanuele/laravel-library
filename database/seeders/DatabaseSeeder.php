24ac32d7150323ff9af832ec69a4110771bc9ee2
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-11-06 16:15:52 +0100

    add FormatSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            FormatSeeder::class,
             GenreSeeder::class,
             BooksTableSeeders::class,
         ]);
